24c5b9e56b40fbb2f5494fb7cdf9097ee5931ed3
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-13 13:52:21 -0800

    commit tree 1a34fcbf8acb60232a8d0085499658320baa1ecf

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driverNames, searchName) {
+    const result = driverNames.filter(name => name.toUpperCase() === searchName.toUpperCase())
+    return result
+}
+
+function fuzzyMatch(driverNames, searchName) {
+    const searchLength = searchName.length
+    const result = driverNames.filter(name => name.substring(0, searchLength) === searchName.substring(0, searchLength))
+    return result
+  }
+
+function matchName(driverList, name) {
+    const result = driverList.filter(driver => driver.name === name)
+    return result
+}
